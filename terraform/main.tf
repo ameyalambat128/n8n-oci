58d0ba686f9a8109a08c30c8eed39e7c25c70d9e
Ameya Lambat <ameyalambat@gmail.com>
2025-09-15 18:54:05 -0700

    Change instance shape to VM.Standard.E2.1.Micro

--- terraform/main.tf
+++ terraform/main.tf
@@ -111,12 +111,7 @@ resource "oci_core_instance" "n8n_instance" {
   availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
   compartment_id      = local.compartment_ocid
   display_name        = "n8n-instance"
-  shape               = "VM.Standard.A1.Flex"
-
-  shape_config {
-    memory_in_gbs = 4
-    ocpus         = 1
-  }
+  shape               = "VM.Standard.E2.1.Micro"
 
   source_details {
     source_type = "image"
